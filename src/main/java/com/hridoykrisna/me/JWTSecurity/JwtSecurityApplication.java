745b51e4aa1baa2232616f96d1920ad0ae01c0fc
hridoy <hridoykrisnadas@gmail.com>
2023-04-14 18:09:05 +0600

    Initial commit

---
+++ src/main/java/com/hridoykrisna/me/JWTSecurity/JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hridoykrisna.me.JWTSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityApplication.class, args);
+	}
+
+}
